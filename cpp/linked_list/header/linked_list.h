72b35b6d4199d1d37159e63e2b0b1187521cec44
Lincoln Fitzsimons <lincoln.f08@gmail.com>
2013-07-23 22:41:41 +1000

    Building and testing

--- cpp/linked_list/header/linked_list.h
+++ cpp/linked_list/header/linked_list.h
@@ -2,44 +2,172 @@
 #ifndef LINKED_LIST_H_
 #define LINKED_LIST_H_
 
-#include<cstddef>
+#include <cstddef>
+#include <iostream>
 
+
+template <class Type>
+class Element {
+    private:
+        Element<Type> *m_next;
+        Type *m_data;
+
+    public:
+        Element();
+        ~Element();
+        
+        // Getters
+        Type *data();
+        Element<Type> *next();
+        
+        // Setters
+        void setData(Type &data);
+        void setNext(Element &element);
+};
+
+template <class Type>
+Element<Type>::Element() {
+    this->m_next = nullptr;
+    this->m_data = nullptr;
+}
+
+template <class Type>
+Element<Type>::~Element() {
+    std::cout << "destroyed\n";
+}
+
+
+template <class Type>
+Type *Element<Type>::data() {
+    return this->m_data;
+}
+
+template <class Type>
+Element<Type> *Element<Type>::next() {
+    return this->m_next;
+}
+
+template <class Type>
+void Element<Type>::setData(Type &data) {
+    this->m_data = &data;
+}
+
+template <class Type>
+void Element<Type>::setNext(Element<Type> &element) {
+    this->m_next = &element;
+}
+
+
+/*!
+ * Linked List Template implementation
+ */
 template <class Type>
 class LinkedList {
     private:
-        struct node {
-            node *next;
-            node *prev;
-            Type *data;
-        };
         
-        node *head;
-        node *tail;
+        Element<Type> *head;
+        int count;
 
     public:
         // constructor
         LinkedList();
         // destructor
-        //~LinkedList();
+        ~LinkedList();
+        
+        // insert elements
+        Element<Type> *push(Type &data);
+        //Element<Type> insert_after(Element &element, Type &data);
+        //Element<Type> insert_before(Element &element, Type &data);
 
-        // insert at end
-        void insert(Type data);
-        // remove 
-        //void remove(Type data);
+        // remove elements 
+        Type *pop();
+        //Type *remove(Element &element);
+        //Type *remove_after(Element &element);
+        //Type *remove_before(Element &element);
+        //void remove_matching(Type &data);
+        
+        // find
+        //Element<Type> *find(Element<Type> &after=nullptr);
+
+        
+        // is list empty
+        bool is_empty() const;
+        // return amount of items in the list
+        int size() const;
 };
+
 
 template <class Type>
 LinkedList<Type>::LinkedList() {
     this->head = nullptr;
-    this->tail = 0;//nullptr;
+    this->count = 0;
 }
 
 template <class Type>
-void LinkedList<Type>::insert(Type data) {
-    node dataNode;
-    dataNode.next = 0;
-    dataNode.prev = 0;
-    dataNode.data = &data;
+LinkedList<Type>::~LinkedList() {
+    Element<Type> *element = this->head;
+    Element<Type> *next = nullptr;
+    while(element != nullptr) {
+        next = element->next();
+        delete element;
+        element = next;
+    }
+}
+
+/*!
+ * push insert Type at the head
+ * @param data The data to be inserted 
+ * @return pointer to the element just inserted
+ */
+template <class Type>
+Element<Type> *LinkedList<Type>::push(Type &data) {
+    Element<Type> *element = new Element<Type>();
+    element->setNext(*(this->head));
+    element->setData(data);
+
+    this->head = element;    
+    this->count++;
+
+    return element;
+}
+
+/*!
+ * pop - remove the head element
+ * @return the data that was inserted
+ */
+template <class Type>
+Type *LinkedList<Type>::pop() {
+    
+    if(this->head != nullptr) {
+        Element<Type> *next = this->head->next();
+        Type *data = this->head->data();
+        delete this->head;
+        this->head = next;
+        this->count--;
+        return data;
+    }
+    else {
+        return nullptr;
+    }
+
+}
+
+/*!
+ * is_empty
+ * @return true if the list is empty
+ */
+template <class Type>
+bool LinkedList<Type>::is_empty() const {
+    return (this->head == nullptr);
+}
+
+/*!
+ *  size
+ *  @return elements in the list
+ */
+template <class Type>
+int LinkedList<Type>::size() const {
+    return this->count;
 }
 
 
--- cpp/linked_list/test/linked_list_test.cpp
+++ cpp/linked_list/test/linked_list_test.cpp
@@ -7,10 +7,52 @@ TEST(LinkedList, Construction) {
     LinkedList<int> list;
 }
 
-TEST(LinkedList, Insert) {
+TEST(LinkedList, Destruction) {
     LinkedList<int> list;
+    
     int test1 = 12;
-    list.insert(test1);
+    list.push(test1);
+}
+
+TEST(LinkedList, Push) {
+    LinkedList<int> list;
+    
+    int test1 = 12;
+    Element<int> *test1Ele = list.push(test1);
+    ASSERT_EQ(test1, *(test1Ele->data()));
+
+    int test2 = 13;
+    Element<int> *test2Ele = list.push(test2);
+    ASSERT_EQ(test2, *(test2Ele->data()));
+}
+
+TEST(LinkedList, IsEmpty) {
+    LinkedList<int> list;
+    
+    ASSERT_TRUE(list.is_empty());
+
+    int test1 = 12;
+    list.push(test1);
+    
+    ASSERT_FALSE(list.is_empty());
+
+}
+
+TEST(LinkedList, Size) {
+    LinkedList<int> list;
+    
+    ASSERT_EQ(0, list.size());
+
+    int test1 = 12;
+    Element<int> *returnedElement = list.push(test1);
+    
+    ASSERT_EQ(1, list.size());
+    
+    list.pop();
+
+    ASSERT_EQ(0, list.size());
+    
+
 }
 
 
